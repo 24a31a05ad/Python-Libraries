6672fc8e7dd56917c1395da38526268f1b42cbcb
Tota Amruta valli <24a31a05ad@pragati.ac.in>
2026-08-30 12:08:50 +0530

    Add files via upload

---
+++ array slicing.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50, 60])
+
+print("Original Array:", arr)
+print("Sliced Array:", arr[1:5])
+print("First 3 elements:", arr[:3])
+print("Last 3 elements:", arr[-3:])
