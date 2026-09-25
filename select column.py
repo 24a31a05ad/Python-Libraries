edfb93fdee43fa332c60e53d5ff1dc2abc4be55b
Tota Amruta valli <24a31a05ad@pragati.ac.in>
2026-09-25 18:22:51 +0530

    Add files via upload

---
+++ select column.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+    "Name": ["Amrutha", "Anu", "Siri"],
+    "Age": [20, 21, 20],
+    "Marks": [85, 90, 88]
+}
+
+df = pd.DataFrame(data)
+
+print("Names:")
+print(df["Name"])
